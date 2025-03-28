eb8c205054a3cdcbba3a49d560e60de2612d639f
dhineshkumar465 <dhineshkumar465@gmail.com>
2025-03-29 02:00:14 +0530

    app index changes

--- app/index.js
+++ app/index.js
@@ -1,25 +1,44 @@
 import { useEffect } from 'react';
 import { Text, View, Image, Pressable } from 'react-native';
 import { Link } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import logo from '../assets/images/logo.png';
+import homeImg from '../assets/images/home_img.png';
+
 import styles from './style';
 
 export default function Dashboard() {
   useEffect(() => {
-    const onReady = async () => {
-      await SplashScreen.preventAutoHideAsync();
-      await SplashScreen.hideAsync();
-    };
-    onReady();
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+
+        // Load images
+        await Asset.loadAsync([require("../assets/images/splash-icon.png")]);
+
+        // Load fonts
+        await Font.loadAsync({
+          "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
   }, []);
 
   return (
     <View style={styles.container}>
       <View style={{ paddingHorizontal: 20 }}>
         <View style={{ alignItems: 'center' }}>
-          <Image style={styles.logoImg} source={require('../assets/images/logo.png')} />
-          <Image style={styles.dashboardImg} source={require('../assets/images/home_img.png')} />
+          <Image style={styles.logoImg} source={logo} />
+          <Image style={styles.dashboardImg} source={homeImg} />
         </View>
         <Text style={styles.text}>Welcome to the
           <Text style={styles.inlineText}> Monitoringweb</Text> ecosystem</Text>
